2663146046e5df6dfa7eea9b31e79e68dbd65a0c
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-16 22:47:48 +0900

    expressの初期プログラム

---
+++ server/server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+const app = express()
+const port = 8080
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
