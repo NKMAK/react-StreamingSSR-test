9de96de53ebace9d49e8ab5914de312440cf9a87
NKMAK <89186723+NKMAK@users.noreply.github.com>
2025-02-20 11:23:40 +0900

    ハイドレーション用のファイル読み込み

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,  // マニフェストファイルの生成
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/main.js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+      }
+    }
+  }
 })
